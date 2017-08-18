6cf55f085328081adacb23e988fedde0d6d209af
Joe Stead <joestead1@gmail.com>
2017-08-18 11:26:16 +0100

    Added everything

---
+++ CodeStopwatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace StopwatchTimer
+{
+    public class CodeStopwatch : IDisposable
+    {
+        private readonly string _codeUnderTime;
+        private readonly Stopwatch _codeStopwatch;
+
+        public CodeStopwatch(string codeUnderTime)
+        {
+            _codeUnderTime = codeUnderTime;
+            _codeStopwatch = new Stopwatch();
+            Console.WriteLine($"Starting {_codeUnderTime}");
+            _codeStopwatch.Start();
+        }
+        
+        public void Dispose()
+        {
+            _codeStopwatch.Stop();
+            Console.WriteLine($"Finished {_codeUnderTime} in {_codeStopwatch.Elapsed}");
+            _codeStopwatch.Reset();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Super simple timer for C#
+Adds a stopwatch around code. Usage:
+
+```csharp
+using (var myTimer = new CodeStopWatch("The thing I'm testing"))
+{
+    //your code
+}
+```
+
+That's it!
