44f92a64ceae182cc21e612ef111eb9b2664b5d6
Joe Stead <joestead1@gmail.com>
2017-09-07 15:46:05 +0100

    Added action overload

--- CodeStopwatch.cs
+++ CodeStopwatch.cs
@@ -3,23 +3,30 @@ using System.Diagnostics;
 
 namespace StopwatchTimer
 {
-    public class CodeStopwatch : IDisposable
+    public class CodeStopWatch : IDisposable
     {
         private readonly string _codeUnderTime;
+        private readonly Action<string> _logAction;
         private readonly Stopwatch _codeStopwatch;
 
-        public CodeStopwatch(string codeUnderTime)
+        public CodeStopWatch(string codeUnderTime) : this(codeUnderTime, Console.WriteLine)
+        {
+        }
+
+        public CodeStopWatch(string codeUnderTime, Action<string> logAction)
         {
             _codeUnderTime = codeUnderTime;
+            _logAction = logAction;
             _codeStopwatch = new Stopwatch();
-            Console.WriteLine($"Starting {_codeUnderTime}");
+            logAction($"Starting {_codeUnderTime}");
             _codeStopwatch.Start();
         }
-        
+
+
         public void Dispose()
         {
             _codeStopwatch.Stop();
-            Console.WriteLine($"Finished {_codeUnderTime} in {_codeStopwatch.Elapsed}");
+            _logAction($"Finished {_codeUnderTime} in {_codeStopwatch.Elapsed}");
             _codeStopwatch.Reset();
         }
     }
--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # Super simple timer for C#
-Adds a stopwatch around code. Usage:
+Adds a stopwatch around code. By default, it will write to stdout. If you would like to log it somewhere else, use the overload and pass in an action.
+ 
+ Usage:
 
 ```csharp
-using (var myTimer = new CodeStopWatch("The thing I'm testing"))
+using (new CodeStopWatch("The thing I'm testing"))
+{
+    //your code
+}
+
+//with an action
+using (new CodeStopWatch("The thing I'm testing", s => _logger.Log(s)))
 {
     //your code
 }
